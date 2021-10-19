b041e3edb8c542cc6cd14ae329891d8ee31250b8
ThirstyBoi <potatogaming999@gmail.com>
2021-10-19 08:04:01 +0800

    Finished Assault Air Tier

--- scripts/main.js
+++ scripts/main.js
@@ -15,7 +15,18 @@ Events.run(Trigger.update, () => {
     type.region.set(regions[index]);
 });
 
-const fx = new Effect(120, e => Angles.randLenVectors(e.id, 4, e.finpow() * 4, extend(Floatc2, {get(x, y){
+const types = [UnitTypes.antumbra, UnitTypes.horizon, UnitTypes.flare, UnitTypes.zenith, UnitTypes.eclipse];
+
+Events.run(Trigger.update, () => Groups.unit.each(u => {
+  if(types.includes(u.type)){
+    fx.at(
+      u.x + Angles.trnsx(u.rotation, -u.type.engineOffset),
+      u.y + Angles.trnsy(u.rotation, -u.type.engineOffset)
+    );
+  };
+}));
+
+const fx = new Effect(20, e => Angles.randLenVectors(e.id, 4, e.finpow() * 4, extend(Floatc2, {get(x, y){
     Draw.z(Layer.blockOver);
     Draw.color(Color.red, Tmp.c2.set(Color.red).mul(0.3), e.fin());
     Fill.circle(e.x + x, e.y + y, 1 + e.fout() * 2);
